f2ad7a250bbf2785b83cb541c69c775059ba3213
adolfoclima <adolfolima@gmail.com>
2022-11-02 16:05:32 -0300

    Adicionado button que liga e desliga led com debounce.

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@
 
 void loop()
 {
-  // static bool ledState; // a variable that keeps the current LED status
-  Button_1.read(); // read the button
-  Button_2.read(); // read the button
+  static bool ledState; // a variable that keeps the current LED status
+  Button_1.read();      // read the button
+  Button_2.read();      // read the button
 
   // if (Button_1.isPressed())
   // {
@@ -19,11 +19,11 @@ void loop()
   //   digitalWrite(PIN_LED_1, false);
   // }
 
-  // if (Button_1.wasPressed()) // if the button was released, change the LED state
-  // {
-  //   ledState = !ledState;
-  //   digitalWrite(PIN_LED_1, ledState);
-  // }
+  if (Button_1.wasPressed()) // if the button was released, change the LED state
+  {
+    ledState = !ledState;
+    digitalWrite(PIN_LED_1, ledState);
+  }
 
   // if (Button_1.wasReleased()) // if the button was released, change the LED state
   // {
@@ -31,15 +31,15 @@ void loop()
   //   digitalWrite(PIN_LED_1, ledState);
   // }
 
-  if (Button_1.pressedFor(2000))
-  {
-    // button has been pressed for one second
-    digitalWrite(PIN_LED_1, true);
-  }
+  // if (Button_1.pressedFor(2000))
+  // {
+  //   // button has been pressed for one second
+  //   digitalWrite(PIN_LED_1, true);
+  // }
 
-  if (Button_2.pressedFor(2000))
-  {
-    // button has been pressed for one second
-    digitalWrite(PIN_LED_1, false);
-  }
+  // if (Button_2.pressedFor(2000))
+  // {
+  //   // button has been pressed for one second
+  //   digitalWrite(PIN_LED_1, false);
+  // }
 }
